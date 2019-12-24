cda57afbd1a4d60cce3279d2852cf022acd2c1cf
githubfoam <lequestionis@gmail.com>
2019-12-24 21:31:02 +0300

    tests update

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
 import unittest
 
-import awesome
+# import awesome
 
 
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
 
+    def smile():
+        return ":)"
+
+    def frown():
+        return ":("
 
 if __name__ == '__main__':
     unittest.main()
